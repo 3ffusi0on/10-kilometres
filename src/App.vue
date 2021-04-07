2cb3d0e5ebad14868bcbca4d33e282c475e7cd38
SIMON Pierre-Alain <pasimon@plussimple.com>
2021-04-07 15:41:04 +0200

    improve address search with autocomplete

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,13 @@
 <template>
   <div id="app">
     <div id="cover">
-      <form method="get" @submit="search" action="#">
-        <div class="tb">
-          <div class="td">
-            <input v-model="address" type="text" placeholder="Rechercher" required />
-          </div>
-          <div class="td" id="s-cover">
-            <button type="submit">
-              <div id="s-circle"></div>
-              <span></span>
-            </button>
-          </div>
-        </div>
-      </form>
+      <autocomplete
+        :search="search"
+        placeholder="Rechercher mon adresse"
+        aria-label="Rechercher mon adresse"
+        :get-result-value="getResultValue"
+        @submit="handleSubmit"
+      ></autocomplete>
     </div>
 
     <div id="map" class="map-cover"></div>
@@ -21,27 +15,28 @@
 </template>
 
 <script>
-import axios from "axios";
+import Autocomplete from "@trevoreyre/autocomplete-vue";
+
 export default {
   name: "App",
+  components: {
+    Autocomplete,
+  },
   data() {
     return {
       circle: null,
       map: null,
       rad: 10000,
-      address: "",
     };
   },
   mounted() {
     var x = 49.6333;
     var y = 5.1667;
 
-    //This code is for simple Map
     this.map = L.map("map");
     this.map.setView([x, y], 11);
-    // add an OpenStreetMap tile layer
-    L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
-      attribution: "© OpenStreetMap contributors",
+    L.tileLayer("https://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+      attribution: "© OpenStreetMap",
     }).addTo(this.map);
     this.circle = new L.circle([x, y], this.rad, {
       color: "green",
@@ -52,35 +47,45 @@ export default {
   },
 
   methods: {
-    search() {
-      axios({
-        method: "get",
-        url: "https://api-adresse.data.gouv.fr/search/",
-        params: {
-          q: this.address,
-        },
-      })
-        .then(
-          (response) => {
-            var coordinates = response.data.features[0].geometry.coordinates || null;
+    search(input) {
+      const url = `https://api-adresse.data.gouv.fr/search/?q=${encodeURI(
+        input
+      )}`;
 
-            if (coordinates) {
-              var x = coordinates[1];
-              var y = coordinates[0];
-              this.map.removeLayer(this.circle);
+      return new Promise((resolve) => {
+        if (input.length < 3) {
+          return resolve([]);
+        }
 
-              this.map.setView([x, y], 11);
-              this.circle = new L.circle([x, y], this.rad, {
-                color: "green",
-                fillColor: "#06A706",
-                fillOpacity: 0.5,
-              });
-              this.map.addLayer(this.circle);
-              this.address = "";
-            }
-          },
-        );
+        fetch(url)
+          .then((response) => response.json())
+          .then((data) => {
+            resolve(data.features);
+          });
+      });
+    },
 
+    getResultValue(result) {
+      return result.properties.label;
+    },
+
+    handleSubmit(result) {
+      this.refreshMap(
+        result.geometry.coordinates[1],
+        result.geometry.coordinates[0]
+      );
+    },
+
+    refreshMap(x, y) {
+      this.map.removeLayer(this.circle);
+
+      this.map.setView([x, y], 11);
+      this.circle = new L.circle([x, y], this.rad, {
+        color: "green",
+        fillColor: "#06A706",
+        fillOpacity: 0.5,
+      });
+      this.map.addLayer(this.circle);
     },
   },
 };
